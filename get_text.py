8d14026d010f60103be62c08736cb380ae62d40b
Grabiel-dargains <54943277+Grabiel-dargains@users.noreply.github.com>
2025-04-24 10:54:07 -0300

    contagem de ocorrências de char

--- get_text.py
+++ get_text.py
@@ -9,3 +9,16 @@ def count(book_str):
         for x in book_str:
             char_count += 1
         print (f"This book has {char_count} characters")
+
+def get_num_words(book_str):
+    palavras = book_str.split()
+    contagem = 0
+    for x in palavras:
+        contagem += 1
+    print(f"{contagem} words found in the book.")
+
+def get_char_count(book_str):
+    char_count = {}
+    for x in book_str.lower():
+        char_count[x] = char_count.get(x, 0) + 1
+    return char_count
--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-from get_text import count, get_book_text
+import get_text as gt
 
 def __main__():
     book_name = input("Type in file name: \n")
-    book_str = get_book_text(f"./books/{book_name}.txt")
+    book_str = gt.get_book_text(f"./books/{book_name}.txt")
     print(book_str)
-    
-    count(book_str)
+    gt.count(book_str)
+    gt.get_num_words(book_str)
+    contagem_caracteres = gt.get_char_count(book_str)
+    print (contagem_caracteres)
 
 __main__()
